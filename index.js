346c7c6f628fe875102df8a8607610907285b16c
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-02-22 23:10:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+  var logShout = "HELLO";
+  if (logShout === string) {
+  return string.toUpperCase();
+  }
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
